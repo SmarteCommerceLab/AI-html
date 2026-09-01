276865e867f39b3cb2155aa7386a64461571a037
deradrea <41356776+deradrea@users.noreply.github.com>
2026-09-01 09:33:33 +0200

    release: align admin navigation in 1.15.11

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
-define('AIHL_VERSION', '1.15.10');
+define('AIHL_VERSION', '1.15.11');
 
 /**
  * Move the visually oversized legacy author banner to the enterprise preset.
